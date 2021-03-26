ac6eb3c57ca229ac0046f2ad2092a5f50baa4bbc
Choi-Jongin <ekejaqutmqk@gmail.com>
2021-03-26 19:42:43 +0900

    add comment

---
+++ CMakeFiles/JEncoder.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JEncoder.dir/JEncoder.cpp.o"
+  "CMakeFiles/JEncoder.dir/main.cpp.o"
+  "JEncoder.pdb"
+  "JEncoder"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JEncoder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
